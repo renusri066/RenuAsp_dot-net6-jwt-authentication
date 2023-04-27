743d889d99563ef233b55612f18de413d541c5aa
renusri066 <renusri066@gmail.com>
2023-04-27 22:42:57 +0530

    first commit

---
+++ Controllers/UsersController.cs
@@ -0,0 +1,21 @@
+﻿namespace WebApi.Controllers;
+
+using Microsoft.AspNetCore.Mvc;
+using WebApi.Authorization;
+using WebApi.Models.Users;
+using WebApi.Services;
+
+[Authorize]
+[ApiController]
+[Route("[controller]")]
+public class UsersController : ControllerBase
+{
+    private IUserService _userService;
+
+    public UsersController(IUserService userService)
+    {
+        _userService = userService;
+    }
+
+    
+}
---
+++ README.md
@@ -0,0 +1,3 @@
+# dotnet-6-jwt-refresh-token
+
+.NET 6.0 - JWT Authentication with Refresh Tokens Tutorial with Example API
